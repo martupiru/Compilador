dad3867ccb2cdf7eecad242e37ae1319b2cc442d
martupiru <martunahman@gmail.com>
2026-03-06 13:07:22 -0300

    maven java

---
+++ code/Compi_AyM/src/main/java/org/compilador/App.java
@@ -0,0 +1,13 @@
+package org.compilador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
